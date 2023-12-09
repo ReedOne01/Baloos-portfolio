91b1d1208de8e568ef51cdc66f2ced6f8ffc5483
ladex lad <ladexlad2@gmail.com>
2023-12-09 23:31:26 +0100

    done

--- app.js
+++ app.js
@@ -10,3 +10,26 @@ btn.addEventListener("click", () => {
   navBarToggler.classList.toggle("active");
   heroSection.classList.toggle("active");
 });
+
+const navbar = document.querySelector(".navbar");
+const navColor = document.querySelector(".navColor");
+const topArrow = document.querySelector(".topArrow");
+
+window.addEventListener("scroll", (e) => {
+  const scrollHeight = window.pageYOffset;
+  const navbarHeight = navbar.getBoundingClientRect().height;
+
+  console.log(navbarHeight, scrollHeight);
+  if (scrollHeight > navbarHeight) {
+    navbar.classList.add("fixed-navbar");
+    navColor.classList.add("showNavColor");
+    // navColor.style.color = "#0c887a";
+  } else {
+    navbar.classList.remove("fixed-navbar");
+  }
+  if (scrollHeight > 100) {
+    topArrow.classList.add("showArrow");
+  } else {
+    topArrow.classList.remove("showArrow");
+  }
+});
